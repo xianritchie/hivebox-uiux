959448dd10ce914c38508fb0ccb4ee7017d8e9be
Christian Ritchie <me@christianritchie.com>
2022-12-30 21:24:10 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function App() {
-  
+  const classes = useStyles();
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
