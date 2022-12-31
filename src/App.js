096d5689770bb3912a072f81ac8dccac682af55f
Christian Ritchie <me@christianritchie.com>
2022-12-30 20:13:42 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,83 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import SettingsIcon from '@material-ui/icons/Settings';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Settings from './Settings';
+import Dashboard from './Dashboard';
 
-const theme = {
-  color: {
-    primary: '#222',
-    secondary: '#333',
-    tertiary: '#444',
-    highlight: '#0077c9',
-    text: '#fff'
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
   },
-  font: {
-    size: 16,
-    family: '"Helvetica Neue", sans-serif'
-  }
-}
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawer: {
+    width: 240,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 240,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
+}));
 
-const Header = () => {
-  return (
-    <header style={{
-      backgroundColor: theme.color.primary,
-      color: theme.color.text,
-      fontSize: theme.font.size,
-      fontFamily: theme.font.family
-    }}>
-      <h1>My App</h1>
-      <Navigation />
-    </header>
-  )
-}
+export default function App() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
 
-const Body = () => {
-  return (
-    <main style={{
-      backgroundColor: theme.color.secondary,
-      color: theme.color.text,
-      fontSize: theme.font.size,
-      fontFamily: theme.font.family
-    }}>
-      <h2>Welcome to My App</h2>
-      <p>This is the body of the app</p>
-    </main>
-  )
-}
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
 
-const Footer = () => {
-  return (
-    <footer style={{
-      backgroundColor: theme.color.tertiary,
-      color: theme.color.text,
-      fontSize: theme.font.size,
-      fontFamily: theme.font.family
-    }}>
-      <p>Copyright 2021 My App</p>
-    </footer>
-  )
-}
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-function App() {
-  <Header />
-  <Body />
-  <Footer />
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-}
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-export default App;
+const handleDrawerToggle = () => {
+  setDrawerOpen(!drawerOpen);
+};
+
+ return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            My App
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="temporary"
+        anchor="left"
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+          <ListItem button component={Link} to="/">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItem>
+          <ListItem button component={Link} to="/dashboard">
+            <ListItemIcon>
+              <DashboardIcon />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItem>
+          <ListItem button component={Link} to="/settings">
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </List>
+        <Divider />
+      </Drawer>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
